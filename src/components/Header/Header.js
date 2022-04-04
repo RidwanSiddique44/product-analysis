9ac295527eb7774c48055cd55c2518d5afc65620
RidwanSiddique44 <srsiddique118@gmail.com>
2022-04-04 11:11:06 +0600

    first commit and navbar added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="ms-auto">
+                        <Nav.Link><Link to="/" className='nav-link'>Home</Link></Nav.Link>
+                        <Nav.Link><Link to="/review" className='nav-link'>Review</Link></Nav.Link>
+                        <Nav.Link><Link to="/dashboard" className='nav-link'>Dashboard</Link></Nav.Link>
+                        <Nav.Link><Link to="/blogs" className='nav-link'>Blogs</Link></Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Reviews/Review.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Review = () => {
+    return (
+        <div>
+            <h1>welcome to review</h1>
+        </div>
+    );
+};
+
+export default Review;
