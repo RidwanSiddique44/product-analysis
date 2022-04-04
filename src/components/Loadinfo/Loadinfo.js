21027bac5667b933f274169200ce89fc266d462f
RidwanSiddique44 <srsiddique118@gmail.com>
2022-04-04 20:07:46 +0600

    reviews added

--- src/components/Loadinfo/Loadinfo.js
+++ src/components/Loadinfo/Loadinfo.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 const Loadinfo = (props) => {
-    const { name } = props.review;
+    const { name, review } = props.review;
     return (
-        <div>
-            <h1>{name}</h1>
-        </div>
+        <Card className="text-center m-5 border border-primary">
+            <Card.Header>Featured</Card.Header>
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>
+                    {review}
+                </Card.Text>
+                <Card.Text>
+                    With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+            </Card.Body>
+
+        </Card>
     );
 };
 
